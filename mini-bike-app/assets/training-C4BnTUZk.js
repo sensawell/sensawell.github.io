b0faa5957d155f1d4beda0a0c3dd28a25a40380d
Gonzalo Zaragoza <gzaragoza@gmail.com>
2025-11-12 23:58:25 +0100

    deleted files

--- mini-bike-app/assets/training-C4BnTUZk.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./level-CcGsBmuk.js";import{k as n}from"./index-z3tDKkID.js";function s(){const e=t();return{currentDayToBeCompleated:n(()=>e.trainings.find(r=>r.isCompleted===!1))}}export{s as u};
