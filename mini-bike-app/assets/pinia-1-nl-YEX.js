521d05c6106053bb638a793df71a717a0c7625bc
Gonzalo Zaragoza <gzaragoza@gmail.com>
2025-11-12 23:39:56 +0100

    updated new dist files

---
+++ mini-bike-app/assets/pinia-1-nl-YEX.js
@@ -0,0 +1,1 @@
+import{c as t}from"./pinia-CpJhHu30.js";import{b as a}from"./index-3fevySbJ.js";import"./index-z3tDKkID.js";const m=a(({app:o})=>{o.use(t())});export{m as default};
---
+++ mini-bike-app/assets/training-C4BnTUZk.js
@@ -0,0 +1,1 @@
+import{a as t}from"./level-CcGsBmuk.js";import{k as n}from"./index-z3tDKkID.js";function s(){const e=t();return{currentDayToBeCompleated:n(()=>e.trainings.find(r=>r.isCompleted===!1))}}export{s as u};
