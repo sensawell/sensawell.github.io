6b0dea9be672e9d38b014958da7a2dd49edcd66d
Gonzalo Zaragoza <gzaragoza@gmail.com>
2025-11-13 16:01:14 +0100

    test

---
+++ mini-bike-app/assets/training-BGT-oKuI.js
@@ -0,0 +1,1 @@
+import{a as t}from"./level-C71HQuuk.js";import{k as n}from"./index-Do6Zuz8U.js";function s(){const e=t();return{currentDayToBeCompleated:n(()=>e.trainings.find(r=>r.isCompleted===!1))}}export{s as u};
