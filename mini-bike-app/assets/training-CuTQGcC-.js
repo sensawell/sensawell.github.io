4b8c05b9e86dc2f39a7c7d433eee714e26fbee55
Gonzalo Zaragoza <gzaragoza@gmail.com>
2025-11-13 22:54:04 +0100

    update version of app with new images route

--- mini-bike-app/assets/training-CuTQGcC-.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./level-Dzxgoowc.js";import{k as n}from"./index-j6DpZtcb.js";function s(){const e=t();return{currentDayToBeCompleated:n(()=>e.trainings.find(r=>r.isCompleted===!1))}}export{s as u};
